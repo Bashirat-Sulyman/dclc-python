39fb4810345ceb12a6802acddb1c5d7eb662fc6e
Bashirat Sulyman <sulymanbashirat14@gmail.com>
2025-04-17 13:09:22 +0100

    Add basic python file with the first Python Exercise

---
+++ basic-python.py
@@ -0,0 +1,9 @@
+first_number = int(input ("Enter first number: "))
+second_number = int(input ("Enter second number: "))
+product = first_number * second_number
+sum = first_number + second_number
+if product <= 1000:
+    print ("The product is:", product)
+else:
+    print("The product is too large. The sum is:", sum)
+    
