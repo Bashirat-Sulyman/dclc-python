9a21ddd7c42786c47629119ac3c6b89eda7b991f
Bashirat Sulyman <sulymanbashirat14@gmail.com>
2025-06-23 11:40:17 +0100

    This is my fourth python exercise

---
+++ Exercise-4.py
@@ -0,0 +1,10 @@
+def remove_chars(pynative, n):
+    return pynative[n:]
+    
+print("Removing characters from a string")
+print(remove_chars("pynative", 4)) 
+
+print(remove_chars("pynative", 2))
+
+print(remove_chars("pynative", 5))
+# output 'native'
